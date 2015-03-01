bc38405eb4a830d76b79d38d7e2f78ef4ded9496
vidklopcic <klopcic.vid@gmail.com>
2015-03-01 18:21:48 +0100

    added support for linux op.

--- PictureOrganizer.py
+++ PictureOrganizer.py
@@ -1,158 +1,175 @@
-# coding=utf-8
-import os
-import time
-import re
-import shutil
-import datetime
-import exifread
+import codecs
+import pickle
+import threading
+import urllib2
+import easygui
 import sys
+import time
+import requests
 
-__author__ = 'vid'
+
+import os
+import wx
+import wx.html2
+import json
+from core import Core
+
+codecs.register(lambda name: codecs.lookup('utf-8') if name == 'cp65001' else None)
+
+try:
+    import uniconsole
+    config_path = os.getenv('appdata')
+    script_path = sys.argv[0]
+except:
+    print 'op sys is linux'
+    script_path = os.path.realpath(__file__)
+    config_path = os.path.dirname(script_path)
+print script_path
+
+class PicturesOrganizer():
+    def __init__(self):
+        self.html_events = {
+            'source_dir_selector': self.source_dir_selector,
+            'output_dir_selector': self.output_dir_selector,
+            'naming_help': self.open_naming_help,
+            'save_setting': self.save_profile,
+            'start_setting': self.start_copying_with_parameters,
+            'load_config': self.load_config  # loads directories from previous usage if there save file
+        }
+        threading.Thread(target=self.add_to_run_counter).start()
+        self.gui_thread()
 
 
-class PictureOrganizer(object):
-    def __init__(self, source_dirs=None, output_dir=None, formats_filter=None, mode='move', remove_duplicates=True,
-                 img_name='{old}', folder_name='{old}', remove_date_from_old_dir_name=False, reference=None,
-                 go_back=False):
-        self.go_back = go_back
-        self.reference = reference
-        self.duplicate_names = [''] + [' (' + str(i) + ')' for i in range(1000)]
-        self.mode = mode
-        self.filter = formats_filter
-        self.recursive = True  # TODO
-        self.remove_duplicate_images = remove_duplicates
+    def add_to_run_counter(self):
+        try:
+            requests.request('GET', 'http://bit.ly/po_stats')
+        except:
+            pass
 
-        # dir naming
-        self.folder_name = folder_name  # %Y-ear %m-onth %d-day %H-hour %M-minute %S-second {old} - old name
-        self.remove_date_from_dir_name = remove_date_from_old_dir_name  # remove everything from beginning to first alphabet character
+    def gui_thread(self):
+        app = wx.App()
+        monitor_size = wx.Display(0).GetGeometry().GetSize()
+        self.dialog = MainWindow(self, None, -1)
+        self.dialog.Show()
+        self.dialog.SetSize((monitor_size[0] / 2, monitor_size[1] / 1.2))
+        self.dialog.Maximize()
+        try:
+            with open(sys.argv[1], 'r') as f:
+                args = pickle.load(f)
+                self.start_copying_with_parameters(args, goback=False)
+        except:
+            self.dialog.browser.LoadURL(
+                'file:///' + os.path.join(os.path.dirname(script_path), 'gui', 'main.html').replace('\\', '/'))
+        app.MainLoop()
 
-        # image naming
-        self.img_name = img_name  # %Y-ear %m-onth %d-day %H-hour %M-minute %S-second {old} - old name | %H;%M ({old})
-        if source_dirs and output_dir:
-            self.rename_and_sort(source_dirs, output_dir)
+    def load_config(self):
+        try:
+            with open(os.path.join(config_path, 'config'), 'r') as f:
+                prev_params = pickle.load(f)
+                self.change_source_dir(prev_params[0][0], '0')
+                for i in range(len(prev_params[0]) - 1):
+                    self.dialog.browser.RunScript('''
+                    $('#source_section').append('<input type="text" class="source_dir" id="source_dir' + src_counter + '"><button class="select_source_dir" onclick="window.location=\'event:source_dir_selector|'+src_counter+\';">...</button><br>');
+                    src_counter ++;
+                    ''')
+                    self.change_source_dir(prev_params[i], str(i))
+                print prev_params
+                self.change_output_dir(prev_params[1])
+        except Exception, e:
+            print e
 
-    def update_gui(self, back=False, close=False):
-        if close:
-            self.reference.dialog.Close()
-        if back:
-            self.reference.dialog.browser.GoBack()
-            return
-        self.reference.dialog.browser.RunScript('''
-        $('#progressbar>div').width('{percent}%');
-        '''.replace('{percent}', self.percentage))
-
-    def rename_and_sort(self, source_dirs, destination):
-        not_coppied = 0
-        if self.remove_duplicate_images:
-            files_list = self.no_duplicates(source_dirs)
-        else:
-            files_list = self.allow_duplicates(source_dirs)
-        for img in files_list:
-            self.percentage = str(float(files_list.index(img)) / len(files_list) * 100.)
-            if self.reference:
-                from wx.richtext import wx
-                wx.CallAfter(self.update_gui)
-            root, filename, extension = img
-            if destination not in root:
-                date_taken = self.get_date_created(os.path.join(root, filename))
-
-                # folder naming
-                new_dir_name = date_taken.strftime(self.folder_name)
-                old_dir_name = os.path.basename(root)
-                if self.remove_date_from_dir_name:
-                    old_dir_name = re.split('^[0-9-_.\s]+', old_dir_name, 1)[-1]
-                try:
-                    new_dir_name = new_dir_name.replace('{old}', old_dir_name).strip()
-                except:
-                    pass
-
-                # img naming
-                new_img_name = date_taken.strftime(self.img_name)
-                try:
-                    new_img_name = new_img_name.replace('{old}', filename.split('.')[0])
-                except:
-                    pass
-                new_img_name += '.' + extension
-
-                # copy / move, mkdirs
-                dest_path = os.path.join(destination, new_dir_name)
-                try:
-                    os.makedirs(dest_path)
-                except Exception, e:
-                    if 'File exists' not in str(e):
-                        print e
-                for i in self.duplicate_names:
-                    try:
-                        print filename + ' --> ' + dest_path, new_img_name + i
-                        if self.mode == 'copy':
-                            shutil.copy(os.path.join(root, filename), os.path.join(dest_path, new_img_name + i))
-                        elif self.mode == 'move':
-                            shutil.move(os.path.join(root, filename), os.path.join(dest_path, new_img_name + i))
-                        break
-                    except Exception, e:
-                        print e
+    def html_catch(self, event):
+        if 'event' in event.GetURL():
+            command = urllib2.unquote(event.GetURL().split('event:')[1])
+            if '|' in command:
+                command, args = command.split('|')
+                self.html_events[command](args)
             else:
-                not_coppied += 1
-        print not_coppied, 'images not copied'
-        self.percentage = '100'
-        if self.reference:
-            wx.CallAfter(self.update_gui)
-            time.sleep(.2)
-        if self.go_back and self.reference:
-            wx.CallAfter(lambda: self.update_gui(back=True))
-        elif self.reference:
-            self.reference.dialog.Close()
+                self.html_events[event.GetURL().split('event:')[1]]()
+            event.Veto()
 
-    def get_date_created(self, path):
-        with open(path, 'rb') as f:
-            try:
-                date_taken = datetime.datetime.fromtimestamp(time.mktime(
-                    time.strptime(str(exifread.process_file(f)['EXIF DateTimeOriginal']), '%Y:%m:%d %H:%M:%S'))
-                )
-            except:
-                date_taken = datetime.datetime.fromtimestamp(os.path.getmtime(path))
-        return date_taken
+    def source_dir_selector(self, nr):
+        dir = easygui.diropenbox()
+        self.change_source_dir(dir, nr)
 
-    def no_duplicates(self, source_dirs):
-        result = {}
-        counter = 0
-        for source_dir in source_dirs:
-            for root, dirnames, filenames in os.walk(unicode(source_dir)):
-                for filename in filenames:
-                    counter += 1
-                    path = os.path.join(root, filename)
-                    if len(filename.split('.')) > 1:
-                        result[filename + str(os.path.getsize(path))] = [root, filename,
-                                                                         filename.split('.')[-1].lower()]
-                    else:
-                        result[filename + str(os.path.getsize(path))] = [root, filename, '']
-        print 'removed ' + str(counter - len(result)) + ' duplicates'
-        if self.filter:
-            result = [result[i] for i in result if result[i][2] in self.filter]
-        else:
-            result = [result[i] for i in result]
-        return result
 
-    def allow_duplicates(self, source_dirs):
-        result = []
-        for source_dir in source_dirs:
-            for root, dirnames, filenames in os.walk(unicode(source_dir)):
-                for filename in filenames:
-                    if len(filename.split('.')) > 1:
-                        result.append([root, filename, filename.split('.')[-1].lower()])
-                    else:
-                        result.append([root, filename, ''])
-        if self.filter:
-            result = [i for i in result if i[2] in self.filter]
-        return result
+    def output_dir_selector(self):
+        dir = easygui.diropenbox()
+        print dir
+        self.change_output_dir(dir)
+
+    def change_source_dir(self, path, nr):
+        if not path:
+            return
+        self.dialog.browser.RunScript('''
+            $('#source_dir{nr}').val('{value}');
+        '''.replace('{value}', path.replace('\\', '\\\\')).replace('{nr}', nr))
+
+    def change_output_dir(self, path):
+        if not path:
+            return
+        self.dialog.browser.RunScript('''
+            $('#output_dir').val('{value}');
+        '''.replace('{value}', path.replace('\\', '\\\\')))
+
+    def open_naming_help(self):
+        self.helper = HelperWindow(None, -1)
+        self.helper.Show()
+        self.helper.SetSize((500, 500))
+        self.helper.browser.LoadURL(
+            'file:///' + os.path.join(os.path.dirname(script_path), 'gui', 'helper.html').replace('\\', '/'))
+        try: self.helper.Bind(wx.html2.EVT_WEBVIEW_NAVIGATING, self.helper_navigating, self.helper.browser)
+        except: self.helper.Bind(wx.html2.EVT_WEB_VIEW_NAVIGATING, self.helper_navigating, self.helper.browser)
+
+    def helper_navigating(self, event):
+        if not 'helper.html' in event.GetURL():
+            monitor_size = wx.Display(0).GetGeometry().GetSize()
+            self.helper.SetSize((monitor_size[0] / 2, monitor_size[1] / 1.2))
+
+    def start_copying_with_parameters(self, params, goback=True):
+        self.dialog.browser.LoadURL(
+            'file:///' + os.path.join(os.path.dirname(script_path), 'gui', 'progress.html').replace('\\', '/'))
+        with open(os.path.join(config_path, 'config'), 'w') as f:
+            pickle.dump(json.loads(params), f)
+        t = threading.Thread(target=self.working_thread, args=[params, goback])
+        t.start()
+
+    def working_thread(self, params, go_back):
+        time.sleep(0.5)
+        Core(*json.loads(params), reference=self, go_back=go_back)
+        wx.CallAfter(self.kill)
+
+    def kill(self):
+        self.dialog.Close()
+
+    def save_profile(self, params):
+        with open(os.path.join(config_path, 'config'), 'w') as f:
+            pickle.dump(json.loads(params), f)
+        save_file = easygui.filesavebox(default='organize_profile', filetypes=['*.po']) + '.po'
+        with open(save_file, 'w') as f:
+            pickle.dump(params, f)
+        print params
+
+
+class MainWindow(wx.Frame):
+    def __init__(self, pic_org, *args, **kwds):
+        wx.Frame.__init__(self, *args, **kwds)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.browser = wx.html2.WebView.New(self)
+        sizer.Add(self.browser, 1, wx.EXPAND, 10)
+        self.SetSizer(sizer)
+        try: self.Bind(wx.html2.EVT_WEBVIEW_NAVIGATING, pic_org.html_catch, self.browser)
+        except: self.Bind(wx.html2.EVT_WEB_VIEW_NAVIGATING, pic_org.html_catch, self.browser)
+
+
+class HelperWindow(wx.Frame):
+    def __init__(self, *args, **kwds):
+        wx.Frame.__init__(self, *args, **kwds)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        self.browser = wx.html2.WebView.New(self)
+        sizer.Add(self.browser, 1, wx.EXPAND, 10)
+        self.SetSizer(sizer)
 
 
 if __name__ == '__main__':
-    t = PictureOrganizer(mode='move',
-             formats_filter=['jpg', 'mov', 'mp4', 'png', 'wmv', 'avi', 'tif', 'mts', 'jpeg'],
-             remove_duplicates=True,
-             img_name='%H;%M ({old})',
-             folder_name='%Y_%m_%d')
-
-    t.rename_and_sort([r'source_dir'],
-                       r'destination_dir')
+    a = PicturesOrganizer()
